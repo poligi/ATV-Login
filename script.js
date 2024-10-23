a2f9cdb68b6a8f47e04c4f3034ee85a42dab45c6
Giovana Pollo <pollogiih2@gmail.comm>
2024-10-22 22:22:28 -0300

    pad de login

---
+++ script.js
@@ -0,0 +1,27 @@
+/* 
+1. Crie uma página HTML com os campos citados na descrição do
+exercício.
+
+2. Ambos os campos são obrigatórios.
+
+3. Não se preocupe muito com a estilização da página; o foco da
+tarefa é a funcionalidade.
+
+4. Não é necessário adicionar funcionalidade para o botão
+“Cadastre-se”, mas ele deverá constar na página.
+
+5. Crie um evento para submeter o Login ao clicar no botão “Login”.
+
+6. Crie uma função Javascript para tratar o evento e para submeter o
+formulário de Login.
+
+7. Construa a integração da página com o Login do back-end que foi
+desenvolvido nos módulos anteriores.
+
+8. Em caso de sucesso: direcione o cliente para a tela da lista de
+recados.
+
+9. Em caso de erro: Mostre um alerta informando o erro.
+
+10. Não submeta o formulário se algum dos campos não possui valor. 
+*/
